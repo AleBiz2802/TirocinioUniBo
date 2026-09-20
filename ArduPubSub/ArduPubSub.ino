c255cd76e4d008e16b32c756a1e1d3f7650497f8
alessio bizzarri <alessio.bizzarri@studio.unibo.it>
2026-09-20 11:14:19 +0200

    connessione mqtt implementata

--- ArduPubSub/ArduPubSub.ino
+++ ArduPubSub/ArduPubSub.ino
@@ -1,9 +1,41 @@
+#include<WiFi.h>
+#include <PubSubClient.h>
+const char* ssid = "Alessio";
+const char* password = "ale2802_";
+const char* mqttbkr = "172.17.0.1 ";
+WiFiClient clientWiFi;
+PubSubClient client(clientWiFi);
+char* nome = "Acquario1";
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); 
+  WiFi.begin(ssid,password);
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(1000);
+    Serial.println("Connessione al wifi in corso");
+  }
+  Serial.println("Connesso al wifi");
+  client.setServer(mqttbkr,1883);
+  while(!client.connected()){
+    Serial.println("CONNESSIONE AL BROKER MQTT");
+    if(client.connect(nome)){
+      Serial.println("CONNESSIONE EFFETTUATA");
+    }else{
+      Serial.print("Fallito, rc = ");
+      Serial.print(client.state());
+      Serial.println("Riprovare"); 
+      delay(5000);
+    }
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(!client.connected()){
+    Serial.println("Riconnessione al broker ");
+    client.connect(nome);
+  }
+  client.publish("test/mqtt","Messaggio di test da arduino giga"); 
+  Serial.println("Messaggio inviato"); 
+  delay(5000); 
+  client.loop();
 }
