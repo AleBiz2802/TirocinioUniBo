6c1f157d013f70a5a39d4f3a31057afaf5611896
alessio bizzarri <alessio.bizzarri@studio.unibo.it>
2026-09-21 13:39:17 +0200

    implementazione callback

--- ArduPubSub/ArduPubSub.ino
+++ ArduPubSub/ArduPubSub.ino
@@ -1,11 +1,24 @@
 #include<WiFi.h>
 #include <PubSubClient.h>
-const char* ssid = "Alessio";
-const char* password = "ale2802_";
-const char* mqttbkr = "172.17.0.1 ";
+#include "TOPIC.h"
+const char* ssid = "Veradsl-Bizzarri";
+const char* password = "15172830";
+const char* mqttbkr = "192.168.0.34";
 WiFiClient clientWiFi;
 PubSubClient client(clientWiFi);
 char* nome = "Acquario1";
+
+
+
+/* Spazio per sensori e valori */
+float ph = 8.0;
+float livelloAcqua = 50.0;
+float temperatura  = 28.0;
+
+
+
+
+/*Spazio per attuatori*/
 void setup() {
   Serial.begin(115200); 
   WiFi.begin(ssid,password);
@@ -15,10 +28,12 @@ void setup() {
   }
   Serial.println("Connesso al wifi");
   client.setServer(mqttbkr,1883);
+  client.setCallback(callback);
   while(!client.connected()){
     Serial.println("CONNESSIONE AL BROKER MQTT");
     if(client.connect(nome)){
       Serial.println("CONNESSIONE EFFETTUATA");
+       client.subscribe("acquari/acquario1/attuatori/#");
     }else{
       Serial.print("Fallito, rc = ");
       Serial.print(client.state());
@@ -34,8 +49,48 @@ void loop() {
     Serial.println("Riconnessione al broker ");
     client.connect(nome);
   }
-  client.publish("test/mqtt","Messaggio di test da arduino giga"); 
+  client.loop();
+ //client.publish("test/mqtt","Messaggio di test da arduino giga");
+  publishFloatVals(client,ph,PH);
+  publishFloatVals(client,livelloAcqua,LIVELLO_ACQUA);
+  publishFloatVals(client,temperatura,TEMPERATURA);
+
+
   Serial.println("Messaggio inviato"); 
   delay(5000); 
-  client.loop();
 }
+
+void publishFloatVals(PubSubClient& t, float val, const char* topic) {
+    char msg[20];
+    snprintf(msg, sizeof(msg), "%1.2f", val);
+    t.publish(topic, msg);
+}   
+
+void callback(char* topic, byte* payload, unsigned int length) {
+    String msg;
+    for (unsigned int i = 0; i < length; i++) {
+        msg += (char)payload[i];
+    }
+
+    Serial.print("Ricevuto su: ");
+    Serial.print(topic);
+    Serial.print(" → ");
+    Serial.println(msg);
+
+    if (strcmp(topic, POMPAR) == 0) {
+        // pompa a immersione
+       // digitalWrite(PIN_POMPAR, msg == "ON" ? HIGH  
+       Serial.println("POMPA RIEMPIMENTO: ");Serial.print(msg+"\n");
+
+    } else if (strcmp(topic, POMPAS) == 0) {
+        // pompa a superficie
+       // digitalWrite(PIN_POMPAS, msg == "ON" ? HIGH : LOW);
+       Serial.println("POMPA SVUOTAMENTO: "); Serial.print(msg+"\n");
+
+    } else if (strcmp(topic, RISCALDATORE) == 0) {
+        // riscaldatore
+       // digitalWrite(PIN_RISC, msg == "ON" ? HIGH : LOW);
+       Serial.println("RISCALDATORE: "); Serial.print(msg+"\n");
+       
+      }
+}   
---
+++ ArduPubSub/TOPIC.h
@@ -0,0 +1,13 @@
+
+//CANALI DI INVIO DEI DATI
+#define PH "acquari/acquario1/sensori/ph"
+#define LIVELLO_ACQUA "acquari/acquario1/sensori/lvl"
+#define TEMPERATURA "acquari/acquario1/sensori/tmp"
+#define TEMPO "acquari/acquario1/sensori/time"
+
+//CANALI DI CONTROLLO DEGLI ATTUATORI
+#define POMPAR "acquari/acquario1/attuatori/pr"
+#define POMPAS "acquari/acquario1/attuatori/ps"
+#define RISCALDATORE "acquari/acquario1/attuatori/risc"
+#define ILLUMINAZIONE "acquari/acquario1/attuatori/luce"
+//CANALI PER I FLAG
