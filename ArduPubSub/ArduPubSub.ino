35687d2dbbd397f66f7a39565560d65850ed421b
alessio bizzarri <alessio.bizzarri@studio.unibo.it>
2026-09-23 12:12:13 +0200

    modulo bluetooth implementato

--- ArduPubSub/ArduPubSub.ino
+++ ArduPubSub/ArduPubSub.ino
@@ -11,14 +11,17 @@ BLEService dataService("19B10010-E8F2-537E-4F6C-D104768A1214");
 BLEStringCharacteristic dataWritingCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214",BLEWrite,100);
 //caratteristica per notificare che i dati sono stati inseriti
 BLEByteCharacteristic dataNotifyingCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214",BLERead | BLENotify);
+
 /* IMPOSTAZIONI WIFI*/
 bool wifiConfigurato = false;
-const char* ssid = "";
-const char* password = "";
+bool mqttConfigurato = false;
+bool mqttAttivato = false;
+String ssid = "";
+String password = "";
 
 /*IMPOSTAZIONI MQTT*/
-const char* mqttbkr = "";
-char* nome = "";
+String mqttbkr = "";
+String nome = "";
 
 WiFiClient clientWiFi;
 PubSubClient client(clientWiFi);
@@ -36,20 +39,38 @@ int check = 0;
 void setup() {
   Serial.begin(115200);
 
- 
+  if(!BLE.begin()){
+    Serial.println("Inizializzazione del modulo Bluetooth® Low Energy fallita"); 
+    while(1);
+  }else{
+    Serial.println("Inizializzazione del modulo Bluetooth® Low Energy completata"); 
+  }
+
+  BLE.setLocalName("AcquarioMQTT");
+  BLE.setAdvertisedService(dataService);
+  
+  dataService.addCharacteristic(dataWritingCharacteristic);
+  dataService.addCharacteristic(dataNotifyingCharacteristic);
+  BLE.addService(dataService);
+  
+  BLE.advertise();
+  Serial.println("Acquario in attesa di connessioni"); 
+
+  /*
+
   WiFi.begin(ssid,password);
   while(WiFi.status()!=WL_CONNECTED){
     delay(1000);
     Serial.println("Connessione al wifi in corso");
   }
   Serial.println("Connesso al wifi");
-  client.setServer(mqttbkr,1883);
+  client.setServer(mqttbkr.c_str(),1883);
   client.setCallback(callback);
   while(!client.connected()){
     Serial.println("CONNESSIONE AL BROKER MQTT");
-    if(client.connect(nome)){
+    if(client.connect(nome.c_str())){
       Serial.println("CONNESSIONE EFFETTUATA");
-       client.subscribe(createTopic(nome,8));
+       client.subscribe(createTopic(nome,8).c_str());
     }else{
       Serial.print("Fallito, rc = ");
       Serial.print(client.state());
@@ -57,32 +78,81 @@ void setup() {
       delay(5000);
     }
   }
-
+*/ 
 }
 
 void loop() {
-  if(!client.connected()){
-    Serial.println("Riconnessione al broker ");
-    client.connect(nome);
+  if(wifiConfigurato==false && dataWritingCharacteristic.written()){
+    String dati = dataWritingCharacteristic.value();
+    ssid     = tokenize(dati, ',', 0);
+    password = tokenize(dati, ',', 1);
+    mqttbkr  = tokenize(dati, ',', 2);
+    nome     = tokenize(dati, ',', 3);
+    dataNotifyingCharacteristic.writeValue(1);
+    wifiConfigurato = true;
+    delay(500);
+    BLE.disconnect();
+    BLE.stopAdvertise();
+    BLE.end();
+    delay(500);
+    WiFi.begin(ssid.c_str(),password.c_str());
+    
   }
-  if(check == 0){
-    client.publish("acquari/nome",nome);
+  if(WiFi.status()!= WL_CONNECTED && wifiConfigurato == true){
+    Serial.println("Connessione al wifi in corso");
+    delay(1000);
   }
-  client.loop();
- //client.publish("test/mqtt","Messaggio di test da arduino giga");
-  publishFloatVals(client,ph,createTopic(nome,0));
-  publishFloatVals(client,livelloAcqua,createTopic(nome,1));
-  publishFloatVals(client,temperatura,createTopic(nome,2));
+  if(WiFi.status()==WL_CONNECTED ){
+    Serial.println("Connesso al wifi");
+    if(mqttConfigurato == false){
+       client.setServer(mqttbkr.c_str(),1883);
+       client.setCallback(callback);
+       mqttConfigurato = true;
+    }
+    if(mqttConfigurato == true){
+      if(!client.connected() && mqttAttivato == false){
+        Serial.println("CONNESSIONE AL BROKER MQTT");
+        if(client.connect(nome.c_str())){
+          Serial.println("CONNESSIONE EFFETTUATA");
+          client.subscribe(createTopic(nome,8).c_str());
+          mqttAttivato = true;
+        }else{
+          Serial.print("Fallito, rc = ");
+          Serial.print(client.state());
+          Serial.println("Riprovare"); 
+          delay(5000);
+        }
+      }
+      if(mqttAttivato == true){
+        if(!client.connected()){
+          Serial.println("Riconnessione al broker ");
+          client.connect(nome.c_str());
+        }
+        if(check == 0){
+          client.publish("acquari/nome",nome.c_str());
+        }
+        client.loop();
+        //client.publish("test/mqtt","Messaggio di test da arduino giga");
+        publishFloatVals(client,ph,createTopic(nome,0));
+        publishFloatVals(client,livelloAcqua,createTopic(nome,1));
+        publishFloatVals(client,temperatura,createTopic(nome,2));
 
 
-  Serial.println("Messaggio inviato"); 
-  delay(5000); 
+        Serial.println("Messaggio inviato"); 
+        delay(5000); 
+
+      }
+    }
+  }
+  
+  
+  
 }
 
-void publishFloatVals(PubSubClient& t, float val, const char* topic) {
+void publishFloatVals(PubSubClient& t, float val, String topic) {
     char msg[20];
     snprintf(msg, sizeof(msg), "%1.2f", val);
-    t.publish(topic, msg);
+    t.publish(topic.c_str(), msg);
 }   
 
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -91,32 +161,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
         msg += (char)payload[i];
     }
 
+    String topicStr = String(topic);
+
     Serial.print("Ricevuto su: ");
-    Serial.print(topic);
-    Serial.print(" → ");
+    Serial.print(topicStr);
+    Serial.print(" -> ");
     Serial.println(msg);
 
-    if (strcmp(topic, createTopic(nome,4)) == 0) {
+    if (topicStr == createTopic(nome,4)) {
         // pompa a immersione
-       // digitalWrite(PIN_POMPAR, msg == "ON" ? HIGH  
-       Serial.println("POMPA RIEMPIMENTO: ");Serial.print(msg+"\n");
+       // digitalWrite(PIN_POMPAR, msg == "ON" ? HIGH : LOW);
+       Serial.println("POMPA RIEMPIMENTO: " + msg);
 
-    } else if (strcmp(topic, createTopic(nome,5)) == 0) {
+    } else if (topicStr == createTopic(nome,5)) {
         // pompa a superficie
        // digitalWrite(PIN_POMPAS, msg == "ON" ? HIGH : LOW);
-       Serial.println("POMPA SVUOTAMENTO: "); Serial.print(msg+"\n");
+       Serial.println("POMPA SVUOTAMENTO: " + msg);
 
-    } else if (strcmp(topic, createTopic(nome,6)) == 0) {
+    } else if (topicStr == createTopic(nome,6)) {
         // riscaldatore
        // digitalWrite(PIN_RISC, msg == "ON" ? HIGH : LOW);
-       Serial.println("RISCALDATORE: "); Serial.print(msg+"\n");
+       Serial.println("RISCALDATORE: " + msg);
        
       }
 }
 
-char* createTopic(char* nome, int pos) {
-    static char topic[100];
-
+String createTopic(String nome, int pos) {
     const char* suffix[9] = {
         "sensori/ph",
         "sensori/lvl",
@@ -129,7 +199,24 @@ char* createTopic(char* nome, int pos) {
         "attuatori/#"
     };
 
-    snprintf(topic, sizeof(topic), "acquari/%s/%s", nome, suffix[pos]);
+    return "acquari/" + nome + "/" + String(suffix[pos]);
+}
 
-    return topic;
+// Estrae il campo in posizione "pos" da toTokenize, usando c come separatore.
+// pos parte da 0. Ritorna "" se pos non esiste.
+String tokenize(String toTokenize, char c, int pos){
+  int inizio = 0;
+  int campo = 0;
+
+  for (int i = 0; i <= toTokenize.length(); i++) {
+    if (i == toTokenize.length() || toTokenize[i] == c) {
+      if (campo == pos) {
+        return toTokenize.substring(inizio, i);
+      }
+      campo++;
+      inizio = i + 1;
+    }
+  }
+
+  return ""; // posizione non trovata
 }
